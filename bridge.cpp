d5daa1f8d00e58aaad2a9660c426689c8d347e59
buratino <buratino@fake>
2016-03-14 19:53:58 -0400

    Sample parsing of iface file for bridge.cpp

--- bridge.cpp
+++ bridge.cpp
@@ -13,14 +13,44 @@
 #include <string.h>
 #include <netdb.h>
 #include <arpa/inet.h>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include "string_utils.h"
 /*----------------------------------------------------------------*/
 
+using namespace std;
 
 /* bridge : recvs pkts and relays them */
 /* usage: bridge lan-name max-port */
 int 
 main (int argc, char *argv[])
 {
+	// Test parsing interface file
+	ifstream ifaceFile("./ifaces/ifaces.a");
+	
+	if(!ifaceFile.is_open()) {
+		cout << "Error opening interface file." << endl;
+		exit(1);
+	}
+	
+	vector< vector<string> > ifaces;
+	
+	while(!ifaceFile.eof()) {
+		string iface;
+		getline(ifaceFile, iface);
+		ifaces.push_back(split(iface, ' '));
+	}
+	
+	for(int i = 0; i < ifaces.size(); ++i) {
+		for(int j = 0; j < ifaces[i].size(); ++j) {
+			cout << ifaces[i][j] << " ";
+		}
+		cout << endl;
+	}
+	
 	/* create the symbolic links to its address and port number
 	* so that others (stations/routers) can connect to it
 	*/
