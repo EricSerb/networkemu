2e67d6cbf20756138963de124aa209e15dcb2689
buratino <buratino@fake>
2016-03-18 21:18:12 -0400

    STATION: now parsing iface files seemingly correctly

--- station.cpp
+++ station.cpp
@@ -1,31 +1,89 @@
 /*-------------------------------------------------------*/
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/time.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <netdb.h>
 #include <arpa/inet.h>
-#include <signal.h>
-#include <sys/wait.h>
-#include <errno.h>
-#include <string.h>
-#include <strings.h>
-#include <stdlib.h>
-
+ 
 #include <cstdlib>
 #include <iostream>
 #include <fstream>
+#include <iostream>
 #include <vector>
 #include <string>
 #include <cstring>
+#include <sstream>
 #include "string_utils.h"
+#include <iomanip>
+#include "ip.h"
 
 using namespace std;
 /*----------------------------------------------------------------*/
 
+void dumpInterfaces(vector<iface> ifaces)
+{
+	for(int i = 0; i < ifaces.size(); ++i) {
+		cout << "Name: " << ifaces[i].ifacename << endl;
+		cout << "IP Address (network byte order): " << ifaces[i].ipaddr << endl;
+		cout << "Subnet Mask (network byte order): " << ifaces[i].mask << endl;
+		cout << "Mac Address: ";
+		for (int j = 0; j < 6; j++)
+			cout << setfill('0') << setw(2) << hex << static_cast<int>(ifaces[i].macaddr[j]) << ":";
+		cout << endl;
+		cout << "Lan Name: " << ifaces[i].lanname << endl;
+		cout << endl;
+	}
+}
+
+vector<iface> extractInterfaces(string fn)
+{
+	// Test parsing interface file
+	ifstream ifaceFile(fn.c_str());
+	
+	if(!ifaceFile.is_open()) {
+		cout << "Error opening interface file." << endl;
+		exit(1);
+	}
+	
+	//vector< vector<string> > lines;
+	vector<iface> ifaces;
+
+	// Parse interface file, which is split by whitespaces
+	string line;
+	while(getline(ifaceFile, line)) {
+		cout << __LINE__ << endl;
+		stringstream linestream(line);
+		string name;
+		string ip;
+		string subnet;
+		string mac;
+		string lan;
+		linestream >> name >> ip >> subnet >> mac >> lan;
+		cout << name << " " << ip << " " << subnet << " "  << mac << " "  << lan << endl;
+		
+		iface interface;
+		strcpy(interface.ifacename, name.c_str());
+		
+		struct sockaddr_in sa;
+		inet_pton(AF_INET, ip.c_str(),&(sa.sin_addr));
+		interface.ipaddr = sa.sin_addr.s_addr;
+		
+		inet_pton(AF_INET, subnet.c_str(),&(sa.sin_addr));
+		interface.mask = sa.sin_addr.s_addr;
+		
+
+		unsigned int iMac[6];
+		int i;
+
+		sscanf(mac.c_str(), "%x:%x:%x:%x:%x:%x", &iMac[0], &iMac[1], &iMac[2], &iMac[3], &iMac[4], &iMac[5]);
+		for(i = 0;i < 6;i++)
+			interface.macaddr[i] = (unsigned char)iMac[i];
+
+			
+		
+		strcpy(interface.lanname, lan.c_str());
+		
+		ifaces.push_back(interface);
+	}
+	
+	return ifaces;
+}
 
 /*----------------------------------------------------------------*/
 /* station : gets hooked to all the lans in its ifaces file, sends/recvs pkts */
@@ -33,7 +91,7 @@ using namespace std;
 int main (int argc, char *argv[])
 {
 	if(argc != 5) {
-		cout << "Usage: station <router flag> <interface> <routing table> <hostname>" << endl;
+		cout << "Usage: station <router flag> <interface file> <routing table> <hostname>" << endl;
 		exit(1);
 	}
 	
@@ -48,29 +106,11 @@ int main (int argc, char *argv[])
 		cout << "Router flag must be `-no` or `-station`" << endl;
 		exit(1);
 	}
+	string fn(argv[2]);
+	vector<iface> ifaces = extractInterfaces(fn);
 	
-	// Test parsing interface file
-	ifstream ifaceFile("./ifaces/ifaces.a");
+	dumpInterfaces(ifaces);
 	
-	if(!ifaceFile.is_open()) {
-		cout << "Error opening interface file." << endl;
-		exit(1);
-	}
-	
-	vector< vector<string> > ifaces;
-	
-	while(!ifaceFile.eof()) {
-		string iface;
-		getline(ifaceFile, iface);
-		ifaces.push_back(split(iface, ' '));
-	}
-	
-	for(int i = 0; i < ifaces.size(); ++i) {
-		for(int j = 0; j < ifaces[i].size(); ++j) {
-			cout << ifaces[i][j] << " ";
-		}
-		cout << endl;
-	}
 	/* initialization of hosts, interface, and routing tables */
 
 	/* hook to the lans that the station should connected to
@@ -84,6 +124,8 @@ int main (int argc, char *argv[])
 	*
 	* for a router, it may need to forward the IP packet
 	*/
+	
+	return 0;
 }
 
 
