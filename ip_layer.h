a3708e9340fd55a33daf596d9bd8da95523f62e5
Eric <els16@my.fsu.edu>
2016-04-04 20:54:36 -0400

    Added bytestoARPpkt so we can convert from arp request packet to arp reply packet

--- ip_layer.h
+++ ip_layer.h
@@ -3,7 +3,11 @@
 
 #include "ip.h"
 #include <vector>
+#include <cstring>
 
 std::vector<unsigned char> writeArpPktToBytes(ARP_PKT pkt);
+
+
+ARP_PKT writeBytesToArpPkt(char *buffer, IPAddr ip, MacAddr mac);
 
 #endif
